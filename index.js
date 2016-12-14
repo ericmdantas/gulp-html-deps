cd14d327ae5ea2c53034b50b3a9aa1c448678788
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-12-14 15:39:19 -0200

    _fixture

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-let HtmlPath = require('./lib')
-let x = new HtmlPath()
-
-x.on('a', (info) => {
-  console.log(info)
-})
-
-x.on('e', (err) => {
-  console.log(err)
-})
-
-x.parse()
+module.exports = require('./lib')
--- test/parser_test.js
+++ test/parser_test.js
@@ -1,8 +1,21 @@
 const {expect} = require('chai');
 const sinon = require('sinon')
 
-describe('parser', () => {
-  it('should', () => {
-    expect(true).to.be.true
+const {parse} = require('../')
+
+describe('parser', () =>  {
+  it('should call the error and end event, html path not specified', () => {
+    let _path = ''
+    let _errorCalled = false
+    let _endCalled = false
+
+    parse(_path).on('error', () => {
+      _errorCalled = true
+    }).on('end', () => {
+      _endCalled = true
+    })
+
+    expect(_errorCalled).to.be.true
+    expect(_endCalled).to.be.true
   })
 })
